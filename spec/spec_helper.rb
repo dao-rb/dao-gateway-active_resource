9bc25fc8d76122fc4f590a7813eb5c42d88ec18c
Александр Фомин <ll.wg.bin@gmail.com>
2016-07-02 18:49:19 +0300

    codeclimate settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'dao/gateway/active_resource'
 require 'rspec/its'
 require 'dao/entity'
