e35ccabac61499c650aeadaeda5ab5d0ed235ba9
Александр Фомин <ll.wg.bin@gmail.com>
2016-07-28 17:59:27 +0300

    transformer support processor

--- lib/dao/gateway/active_resource/base_transformer.rb
+++ lib/dao/gateway/active_resource/base_transformer.rb
@@ -2,24 +2,14 @@ module Dao
   module Gateway
     module ActiveResource
       class BaseTransformer < Dao::Gateway::ScopeTransformer
-        def many(relation)
-          super(relation)
-        end
-
         def one(relation)
           super(Array(relation))
         end
 
         private
 
-        def transform(relation)
-          super(relation) do |record|
-            collect_attributes(record)
-          end
-        end
-
-        def collect_attributes(record)
-          HashWithIndifferentAccess.new(record.try(:serializable_hash))
+        def add_processors
+          @processors.unshift ResourceProcessor.new
         end
       end
     end
