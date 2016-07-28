d3f41cda34b08d2eb1a4717974cfc6a94a7894cd
Александр Фомин <ll.wg.bin@gmail.com>
2016-07-28 17:59:09 +0300

    require processor

--- lib/dao/gateway/active_resource.rb
+++ lib/dao/gateway/active_resource.rb
@@ -4,6 +4,7 @@ require 'dao/gateway/active_resource/version'
 require 'active_resource'
 require 'active_support/hash_with_indifferent_access'
 
+require 'dao/gateway/active_resource/resource_processor'
 require 'dao/gateway/active_resource/base'
 require 'dao/gateway/active_resource/base_transformer'
 
