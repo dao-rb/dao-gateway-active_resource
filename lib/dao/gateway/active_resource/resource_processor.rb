83e2f8557ba86260a52af242f29a2c0f1d7b6013
Александр Фомин <ll.wg.bin@gmail.com>
2016-07-28 17:58:53 +0300

    resource processor

---
+++ lib/dao/gateway/active_resource/resource_processor.rb
@@ -0,0 +1,11 @@
+module Dao
+  module Gateway
+    module ActiveResource
+      class ResourceProcessor < Gateway::Processor
+        def process(record, *)
+          HashWithIndifferentAccess.new(record.try(:serializable_hash))
+        end
+      end
+    end
+  end
+end
